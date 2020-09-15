56cc3dd915e2a3d7df81795e5f4cd19f3faba3e1
Magnus Sagerup <magnus.sagerup@gmail.com>
2020-09-15 12:34:27 +0200

    'working'

--- 4/ios/Flutter/flutter_export_environment.sh
+++ 4/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ploon\Documents\code\FlutterBootcamp\4"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Magnus\Documents\Code\flutterCode\flutterPlayground\4"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
--- 4/lib/main.dart
+++ 4/lib/main.dart
@@ -5,22 +5,35 @@ void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
 
-  Widget renderButtons () {
-    int amount = 5;
-    for(var i = 0; i < amount; i++) {
-      return Text('hello $i');
-    }
-  }
+  // Widget renderButtons () {
+  //   int amount = 5;
+  //   for(var i = 0; i < amount; i++) {
+  //     return Text('hello $i');
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
+    List colorBars = [Colors.orange, Colors.yellow, Colors.green, Colors.teal, Colors.red, Colors.blue, Colors.purple];
+    var player = AudioCache();
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Row(
-            children: [
-              RenderButtons()
-            ],
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: List.generate(colorBars.length, (i) {
+                var notes = i +1;
+                return Expanded(
+                  child: Container(
+                    color: colorBars[i],
+                    child: FlatButton(
+                      onPressed: () {
+                        player.play('note$notes.wav');
+                      },
+                    )
+                  ),
+                );
+              }),
           )
         ),
       ),
